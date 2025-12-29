3227700c37f0353f410ea86e8b5c46857024beb6
lja3723 <lja3723@gmail.com>
2025-12-29 17:25:19 +0900

    feat: 테스트 초기화 테스트를 위한 기본적인 경로 생성 및 파일 추가

---
+++ src/test/java/db/DatabaseTest.java
@@ -0,0 +1,30 @@
+package db;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DatabaseTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void addUser() {
+    }
+
+    @Test
+    void findUserById() {
+    }
+
+    @Test
+    void findAll() {
+    }
+}
---
+++ src/test/java/model/UserTest.java
@@ -0,0 +1,38 @@
+package model;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UserTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getUserId() {
+    }
+
+    @Test
+    void getPassword() {
+    }
+
+    @Test
+    void getName() {
+    }
+
+    @Test
+    void getEmail() {
+    }
+
+    @Test
+    void testToString() {
+    }
+}
