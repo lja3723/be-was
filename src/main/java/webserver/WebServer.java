9e818c3805f808131106f73e73cc60d5ceaac97d
lja3723 <lja3723@gmail.com>
2026-01-01 23:24:45 +0900

    [STEP1] refactor: Thread 대신 concurrent 패키지 사용

--- src/main/java/webserver/WebServer.java
+++ src/main/java/webserver/WebServer.java
@@ -2,6 +2,8 @@ package webserver;
 
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -10,7 +12,11 @@ public class WebServer {
     private static final Logger logger = LoggerFactory.getLogger(WebServer.class);
     private static final int DEFAULT_PORT = 8080;
 
-    public static void main(String args[]) throws Exception {
+    // 고정 크기 스레드 풀
+    private static final int THREAD_POOL_SIZE = 50;
+    private static final ExecutorService executor = Executors.newFixedThreadPool(THREAD_POOL_SIZE);
+
+    public static void main(String[] args) throws Exception {
         int port = 0;
         if (args == null || args.length == 0) {
             port = DEFAULT_PORT;
@@ -22,12 +28,13 @@ public class WebServer {
         try (ServerSocket listenSocket = new ServerSocket(port)) {
             logger.info("Web Application Server started {} port.", port);
 
-            // 클라이언트가 연결될때까지 대기한다.
             Socket connection;
             while ((connection = listenSocket.accept()) != null) {
-                Thread thread = new Thread(new RequestHandler(connection));
-                thread.start();
+                // Thread 생성 대신 작업 제출
+                executor.execute(new RequestHandler(connection));
             }
+        } finally {
+            executor.shutdown();
         }
     }
 }
