942559ef5904ade053a8ed832d7c291898e3a0bc
lja3723 <lja3723@gmail.com>
2026-01-05 17:52:32 +0900

    [w1/step2] refactor: http 통신을 오케스트레이션하는 클래스의 이름 및 위치 변경

--- src/main/java/webserver/HttpRequestProcessor.java
+++ src/main/java/webserver/HttpRequestProcessor.java
@@ -1,4 +1,4 @@
-package handler;
+package webserver;
 
 import http.field.HttpRequestUrl;
 import java.io.IOException;
@@ -8,8 +8,7 @@ import java.net.Socket;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import handler.response.HttpResponseHandler;
-import handler.response.SuccessHttpResponseHandler;
+import responsehandler.SuccessHttpResponseHandler;
 import http.field.HttpField;
 import http.header.HttpRequestHeader;
 import http.header.HttpRequestHeaderHead;
@@ -20,10 +19,11 @@ import http.parser.Parser;
  *
  * <p>{@link Socket}으로부터 클라이언트와의 {@link InputStream}과 {@link OutputStream}을 획득한 후
  * 서버가 클라이언트의 HTTP Request에 대한 적절한 Response를 생성하는 거시적인 동작을 담당한다.</p>
+ * <p>클래스 이름은 AI에게 추천 받았습니다.</p>
  */
-public class ClientRequestHandler implements Runnable {
+public class HttpRequestProcessor implements Runnable {
 
-    private static final Logger logger = LoggerFactory.getLogger(ClientRequestHandler.class);
+    private static final Logger logger = LoggerFactory.getLogger(HttpRequestProcessor.class);
 
     private final Socket connection;
     private final Parser<HttpField, String> httpFieldParser;
@@ -37,7 +37,7 @@ public class ClientRequestHandler implements Runnable {
      * @param httpRequestHeaderHeadParser HTTP header head field parser 구현체
      * @param httpRequestUrlParser HTTP request URL parser 구현체
      */
-    public ClientRequestHandler(Socket connection,
+    public HttpRequestProcessor(Socket connection,
                                 Parser<HttpField, String> httpFieldParser,
                                 Parser<HttpRequestHeaderHead, String> httpRequestHeaderHeadParser,
                                 Parser<HttpRequestUrl, String> httpRequestUrlParser) {
@@ -83,15 +83,27 @@ public class ClientRequestHandler implements Runnable {
                 httpFieldParser,
                 httpRequestUrlParser);
             logHttpRequestHeader(httpRequestHeader);
-
-            // HTTP request header를 기반으로 OutputStream에 HttpResponse를 전송하는 작업을 위임함
-            HttpResponseHandler responseHandler = new SuccessHttpResponseHandler(httpRequestHeader, out);
-            responseHandler.handleResponse();
+            routeHttpRequest(httpRequestHeader, out);
 
         } catch (Exception e) {
-            logger.error("error occur: {}, {}", e.getMessage(), e.getCause(), e);
-            // TODO: 서버의 Exception을 확장성 있게 잡아내어 핸들러로 넘겨주는 방식으로 수정 필요, 예외에 따른 적절한 HTTP 응답 처리 로직 구현 필요
+            handleException(e);
         }
+    }
+
+    /**
+     * HTTP request를 적절한 핸들러로 라우팅
+     */
+    // TODO: http request 내용에 따른 다양한 success response handler 라우팅 로직 추가 필요
+    public void routeHttpRequest(HttpRequestHeader httpRequestHeader, OutputStream out) {
+
+        // HTTP request header를 기반으로 OutputStream에 HttpResponse를 전송하는 작업을 위임함
+        // 현재는 모든 요청에 대해 200 응답의 SuccessHttpResponseHandler를 사용하도록 구현되어 있음
+        new SuccessHttpResponseHandler().handleResponse(httpRequestHeader, out);
+    }
+
+    public void handleException(Exception e) {
+        logger.error("error occur: {}, {}", e.getMessage(), e.getCause(), e);
+        // TODO: 서버의 Exception을 확장성 있게 잡아내어 핸들러로 넘겨주는 방식으로 수정 필요, 예외에 따른 적절한 HTTP 응답 처리 로직 구현 필요
     }
 
     /**
--- src/test/java/webserver/HttpRequestProcessorTest.java
+++ src/test/java/webserver/HttpRequestProcessorTest.java
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class ClientRequestHandlerTest {
+class HttpRequestProcessorTest {
 
     @BeforeEach
     void setUp() {
