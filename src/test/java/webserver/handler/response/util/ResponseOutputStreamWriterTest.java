1194a33201138c40fc8d5bf28927f8083a0f7fa3
lja3723 <lja3723@gmail.com>
2026-01-05 15:12:12 +0900

    [w1/step2] test: ResponseOutputStreamWriter 테스트 작성

--- src/test/java/webserver/handler/response/util/ResponseOutputStreamWriterTest.java
+++ src/test/java/webserver/handler/response/util/ResponseOutputStreamWriterTest.java
@@ -1,23 +1,114 @@
 package webserver.handler.response.util;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+import http.ContentType;
+import http.HttpStatus;
+import http.HttpVersion;
+import http.header.HttpResponseHeader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import handler.response.util.ResponseOutputStreamWriter;
 
 class ResponseOutputStreamWriterTest {
 
+    private String body;
+    private HttpResponseHeader responseHeader;
+    private MockDataOutputStream mockDos;
+
     private ResponseOutputStreamWriter responseOutputStreamWriter;
 
     @BeforeEach
     void setUp() {
+        this.body = "<h1>hello</h1>";
+        this.responseHeader = HttpResponseHeader.builder()
+            .version(HttpVersion.HTTP_1_1)
+            .status(HttpStatus.OK)
+            .contentType(ContentType.TEXT_HTML)
+            .body(body.getBytes())
+            .build();
 
-    }
+        this.mockDos = new MockDataOutputStream(new MockOutputStream());
 
-    @Test
-    void encodeHeader() {
+        this.responseOutputStreamWriter = new ResponseOutputStreamWriter(
+            mockDos,
+            responseHeader,
+            body.getBytes()
+        );
     }
 
     @Test
     void flushResponse() {
+        // given & when
+        this.responseOutputStreamWriter.flushResponse();
+
+        // then
+        // 메서드 호출 횟수는 각각 1번이어야 함
+        assertEquals(1, mockDos.getWrite3ArgCallCount());
+        assertEquals(1, mockDos.getFlushCallCount());
+
+        int headerLength = this.responseHeader.encode().length();
+        int bodyLength = this.body.length();
+
+        // 총 작성된 바이트 수는 헤더 길이 + 바디 길이와 같아야 함
+        assertEquals(headerLength + bodyLength, mockDos.getWrittenBytes());
+
+    }
+}
+
+/**
+ * 메서드 호출 횟수 및 작성된 바이트 수를 추적하는 Mock DataOutputStream
+ */
+class MockDataOutputStream extends DataOutputStream {
+
+    private int flushCallCount;
+    private int write3ArgCallCount;
+
+    public MockDataOutputStream(OutputStream out) {
+        super(out);
+        this.flushCallCount = 0;
+        this.write3ArgCallCount = 0;
+    }
+
+    @Override
+    public void write(int b) throws IOException {
+        super.write(b);
+    }
+
+    @Override
+    public synchronized void write(byte[] b, int off, int len) throws IOException {
+        super.write(b, off, len);
+        write3ArgCallCount++;
+    }
+
+    @Override
+    public void flush() {
+        flushCallCount++;
+    }
+
+    public int getWrittenBytes() {
+        // 부모 클래스의 written 필드에 접근하여 작성된 바이트 수 반환
+        return super.written;
+    }
+
+    public int getFlushCallCount() {
+        return flushCallCount;
+    }
+
+    public int getWrite3ArgCallCount() {
+        return write3ArgCallCount;
+    }
+}
+
+/**
+ * MockDataOutputStream에 전달할 단순 OutputStream 구현체
+ */
+class MockOutputStream extends OutputStream {
+
+    @Override
+    public void write(int b) throws IOException {
     }
 }
