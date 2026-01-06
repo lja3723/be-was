abf72aac5ded8d9f107b824b60a4eb621bf46b38
lja3723 <lja3723@gmail.com>
2026-01-06 18:29:54 +0900

    [w1/step3] feat: http field 파싱을 주석 가능한 키와 그렇지 않은 키에 대해 나눠서 처리하도록 함
    TODO: 추후 key에 대응하는 값을 문자열이 아닌 객체로 저장하도록 구현

--- src/main/java/webserver/http/parser/HttpFieldParser.java
+++ src/main/java/webserver/http/parser/HttpFieldParser.java
@@ -1,10 +1,13 @@
 package webserver.http.parser;
 
+import app.exception.BadRequestException;
 import webserver.http.field.HttpField;
 import webserver.http.field.HttpFieldKey;
 
 /**
  * HTTP Header Field의 한 줄을 파싱하는 Parser 구현체
+ * <p>한 줄을 파싱하는 규칙은 <a href="https://www.rfc-editor.org/rfc/rfc9110.html#section-5">RFC 9110,
+ * Section 5</a>를 참고함</p>
  */
 public class HttpFieldParser implements Parser<String, HttpField> {
 
@@ -17,16 +20,32 @@ public class HttpFieldParser implements Parser<String, HttpField> {
     public HttpField parse(String rawFieldLine) {
         String[] split = rawFieldLine.split(":", 2);
 
-//        if (split .length != 2) {
-//            throw new BadRequestException("Invalid HTTP Field Line: " + rawFieldLine);
-//        }
+        if (split .length != 2) {
+            throw new BadRequestException("Invalid HTTP Field Line: " + rawFieldLine);
+        }
 
+        HttpFieldKey fieldKey = HttpFieldKey.fromString(split[0].trim());
 
-        // TODO: 괄호 '()'를 모두 치환후, ','로 split후 ';'로 split. 그리고 치환되었던 ()를 원복하는 알고리즘 구현 예정
+        return switch (fieldKey) {
+            // AI를 활용하여 comment 문법이 사용될 수 있는 모든 필드 유형을 알아내었음
+            // 해당 필드는 value를 통째로 사용, comment를 처리하도록 별도 메서드로 위임
+            case USER_AGENT, SERVER, VIA -> parseCommentableField(fieldKey, split[1]);
+            default -> parseUncommentableField(fieldKey, split[1]);
+        };
+    }
 
+    public HttpField parseCommentableField(HttpFieldKey key, String rawValue) {
         return HttpField.builder()
-            .key(split[0])
-            .value(split[1]) // TODO: 추후 key에 대응하는 값을 문자열이 아닌 객체로 저장하도록 변경
+            .key(key)
+            .value(rawValue.trim())
+            .build();
+    }
+
+    public HttpField parseUncommentableField(HttpFieldKey key, String rawValue) {
+        // ','로 split후 ';'로 split하는 알고리즘 구현 예정
+        return HttpField.builder()
+            .key(key)
+            .value(rawValue) // TODO: 추후 key에 대응하는 값을 문자열이 아닌 객체로 저장하도록 변경
             .build();
     }
 }
