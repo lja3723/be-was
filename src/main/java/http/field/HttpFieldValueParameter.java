14dbfb115548abefaa06e40b3de28283e5c8c3e1
lja3723 <lja3723@gmail.com>
2026-01-06 09:42:53 +0900

    [w1/step3] refactor: 레코드화 가능한 클래스를 record로 변경

--- src/main/java/http/field/HttpFieldValueParameter.java
+++ src/main/java/http/field/HttpFieldValueParameter.java
@@ -1,10 +1,9 @@
 package http.field;
 
 /**
- * <p>HTTP Header Field Value의 Parameter를 표현</p>
+ * <p>HTTP Header Field Value의 Parameter를 표현하는 record</p>
  * 추후 {@link HttpFieldValue}의 parameters로 사용 예정
  */
-public class HttpFieldValueParameter {
-    public String key;
-    public String value;
+public record HttpFieldValueParameter(String key, String value) {
+
 }
