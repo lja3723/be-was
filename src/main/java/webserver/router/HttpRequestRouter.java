9516a8149af0b230ae182eccebafc5817690b7b2
lja3723 <lja3723@gmail.com>
2026-01-06 16:06:52 +0900

    [w1/step3] refactor: HttpRequestRouter가 HttpRequestHeader가 아닌 HttpRequest가 키가 되도록 수정

--- src/main/java/webserver/router/HttpRequestRouter.java
+++ src/main/java/webserver/router/HttpRequestRouter.java
@@ -1,16 +1,16 @@
 package webserver.router;
 
-import webserver.http.header.HttpRequestHeader;
+import webserver.http.HttpRequest;
 import app.responsehandler.HttpResponseHandler;
 import app.responsehandler.SuccessHttpResponseHandler;
 
 /**
  * HttpRequestHeader에 따른 HttpResponseHandler를 라우팅하는 Router
  */
-public class HttpRequestRouter implements Router<HttpRequestHeader, HttpResponseHandler> {
+public class HttpRequestRouter implements Router<HttpRequest, HttpResponseHandler> {
 
     @Override
-    public HttpResponseHandler route(HttpRequestHeader httpRequestHeader) {
+    public HttpResponseHandler route(HttpRequest httpRequest) {
 
         // TODO: 핸들러 로직 구현
         // HTTP request header를 기반으로 OutputStream에 HttpResponse를 전송하는 작업을 위임함
