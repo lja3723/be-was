38c9d98c639a566bd124df97aa59b1c7c0539ea7
lja3723 <lja3723@gmail.com>
2026-01-08 16:58:52 +0900

    [w2/step1] refactor: static loader가 맨 뒤 슬래시 여부, index.html여부 관계없이 리소스 가져오도록 수정

--- src/main/java/webserver/handler/StaticResourceHandler.java
+++ src/main/java/webserver/handler/StaticResourceHandler.java
@@ -2,28 +2,35 @@ package webserver.handler;
 
 import app.exception.InternalServerErrorException;
 import app.exception.ResourceNotFoundException;
+import webserver.http.ContentType;
 import webserver.http.HttpRequest;
 import webserver.http.HttpResponse;
-import webserver.http.uri.HttpRequestUri;
 import java.io.IOException;
 import java.io.InputStream;
 import webserver.http.HttpVersion;
 import webserver.http.HttpStatus;
 import webserver.http.header.HttpResponseHeader;
+import webserver.util.FileExtensionExtractor;
 
 /**
- * HTTP Status가 200번대인 HTTP Response를 핸들링하는 ResponseHandler
+ * HTTP Status가 200번대인 static resource HTTP request를 핸들링하는 RequestHandler
  */
 public class StaticResourceHandler extends HttpRequestHandler {
 
     @Override
     public HttpResponse handleResponse(HttpRequest httpRequest) {
 
-        HttpRequestUri httpRequestUri = httpRequest.header().uri();
-        try (InputStream is = getClass().getClassLoader().getResourceAsStream(httpRequestUri.resourcePath())) {
+        String path = "static" + httpRequest.header().uri().getPath();
+        if (path.endsWith("/")) {
+            path = path.substring(0, path.length() - 1);
+        }
+        if (FileExtensionExtractor.get(path) == null) {
+            path += "/index.html";
+        }
+        try (InputStream is = getClass().getClassLoader().getResourceAsStream(path)) {
             // 요청에 맞는 리소스가 존재하지 않음
             if (is == null) {
-                throw new ResourceNotFoundException("Resource not found: " + httpRequestUri);
+                throw new ResourceNotFoundException("Resource not found: " + path);
             }
             byte[] body = is.readAllBytes();
 
@@ -31,7 +38,8 @@ public class StaticResourceHandler extends HttpRequestHandler {
                 HttpResponseHeader.builder()
                     .version(HttpVersion.HTTP_1_1)
                     .status(HttpStatus.OK)
-                    .contentType(httpRequest.header().uri().contentType())
+                    .contentType(ContentType.fromFileExtension(
+                        FileExtensionExtractor.get(path)))
                     .body(body)
                     .build(),
                 body);
