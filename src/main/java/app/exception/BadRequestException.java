0501553d5bac7932988da12927ce41a19fbda992
lja3723 <lja3723@gmail.com>
2026-01-13 15:46:55 +0900

    [w2/step2] docs: javadoc 추가

--- src/main/java/app/exception/BadRequestException.java
+++ src/main/java/app/exception/BadRequestException.java
@@ -1,5 +1,9 @@
 package app.exception;
 
+/**
+ * 잘못된 요청을 나타내는 예외 클래스
+ * <p>이 예외는 클라이언트가 잘못된 형식의 요청을 보낼 때 던져짐</p>
+ */
 public class BadRequestException extends RuntimeException {
 
     public BadRequestException(String message) {
