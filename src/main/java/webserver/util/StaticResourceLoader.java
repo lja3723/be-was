49f4a8bda183cb660ddf76f93bf617f857730ee0
lja3723 <lja3723@gmail.com>
2026-01-13 23:35:55 +0900

    [w2/step5] refactor: Static Resource Loader가 리소스가 없는 경우 null 반환하도록 변경

--- src/main/java/webserver/util/StaticResourceLoader.java
+++ src/main/java/webserver/util/StaticResourceLoader.java
@@ -1,7 +1,6 @@
 package webserver.util;
 
 import app.exception.InternalServerErrorException;
-import app.exception.ResourceNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 
@@ -14,28 +13,32 @@ public class StaticResourceLoader {
      * 주어진 경로에 해당하는 정적 리소스를 바이트 배열로 로드한다.
      *
      * @param path 리소스의 경로
-     * @return 리소스의 바이트 배열
-     * @throws ResourceNotFoundException    리소스를 찾을 수 없는 경우
+     * @return 리소스의 바이트 배열, 리소스가 존재하지 않으면 null
      * @throws InternalServerErrorException 입출력 오류가 발생한 경우
      */
     public static byte[] loadResource(String path) {
-
-        if (path.endsWith("/")) {
-            path = path.substring(0, path.length() - 1);
-        }
-        if (FileExtensionExtractor.get(path) == null) {
-            path += "/index.html";
-        }
+        path = getStaticResourcePath(path);
 
         try (InputStream is = StaticResourceLoader.class.getClassLoader().getResourceAsStream(path)) {
             // 요청에 맞는 리소스가 존재하지 않음
             if (is == null) {
-                throw new ResourceNotFoundException("Resource not found: " + path);
+                return null;
             }
             return is.readAllBytes();
 
         } catch (IOException e) {
             throw new InternalServerErrorException(e.getMessage(), e);
         }
+    }
+
+    public static String getStaticResourcePath(String path) {
+        path = "static" + path;
+        if (path.endsWith("/")) {
+            path = path.substring(0, path.length() - 1);
+        }
+        if (FileExtensionExtractor.get(path) == null) {
+            path += "/index.html";
+        }
+        return path;
     }
 }
